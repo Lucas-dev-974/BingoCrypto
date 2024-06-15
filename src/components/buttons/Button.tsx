abb130613a7c5b648a574cb0be252994fc193456
lucas <lucas.lvn97439@gmail.com>
2024-06-15 18:06:15 +0500

    define button style, to continue with another style

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,4 +1,4 @@
-import { JSXElement } from "solid-js";
+import { JSXElement, mergeProps } from "solid-js";
 
 import "./Button.css";
 
@@ -6,11 +6,21 @@ interface ButtonProps {
   text: string;
   onClick: () => void;
   class?: string;
+  dark?: boolean;
+  type?: "primary" | "borderless";
 }
 
 export function Button(props: ButtonProps): JSXElement {
+  const mergedProps = mergeProps({ type: "primary", class: "" }, props);
   return (
-    <button onClick={props.onClick} class={props.class + " button"}>
+    <button
+      onClick={props.onClick}
+      class={mergedProps.class + " button btn-" + mergedProps.type}
+      classList={{
+        "text-white": !props.dark,
+        "text-dark": props.dark,
+      }}
+    >
       {props.text}
     </button>
   );
