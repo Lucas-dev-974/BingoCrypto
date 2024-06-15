2468aced23a53263c95a2b9fb9567ce06fefc1eb
lucas <lucas.lvn97439@gmail.com>
2024-06-15 18:07:43 +0500

    upgrade text2 component

--- src/components/texts/Texte2.tsx
+++ src/components/texts/Texte2.tsx
@@ -3,16 +3,19 @@ import { JSXElement, mergeProps } from "solid-js";
 interface Text2Props {
   text: string;
   hideOnMobile?: boolean;
+  dark?: boolean;
 }
 
 export function Text2(props: Text2Props): JSXElement {
-  const mergedProps = mergeProps({ hideOnMobile: false }, props);
+  const mergedProps = mergeProps({ hideOnMobile: false, dark: false }, props);
 
   return (
     <h2
-      class="text-3xl text-white"
+      class="text-3xl"
       classList={{
         "hidden sm:block": mergedProps.hideOnMobile,
+        "text-white": !mergedProps.dark,
+        "text-dark": mergedProps.dark,
       }}
     >
       {props.text}
